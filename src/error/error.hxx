838bfc3eacc2b52624d04c4c9be453e819774263
Ashwin <ashwinchetty@gmail.com>
2017-04-03 23:01:20 -0500

    text to screen

---
+++ src/error/error.hxx
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+#define KYEL  "\x1B[33m"
+#define KRED  "\x1B[31m"
+#define KNRM  "\x1B[0m"
+
+// associate verbosity with filename (but, because we don't have strcmp
+// just yet, just look at the first letter of the filename).
+//
+// this is a bit of a hack; a better way of doing this would involve
+// #defining a VERBOSE_PRECEDENCE flag in each of the associated files,
+// and relying on that.
+
+#define ERR(...) { \
+	fprintf(stderr,KRED); \
+	fprintf(stderr,"At %s: %d\n",__FILE__,__LINE__); \
+	fprintf(stderr, __VA_ARGS__); \
+	fprintf(stderr,KNRM"\n"); \
+}
--- src/world/main.cxx
+++ src/world/main.cxx
@@ -2,27 +2,43 @@
 
 #include <iostream>
 
+#include "../error/error.hxx"
+
 // #include "world.hxx"
 
 int main(){
-	
+    using namespace std;
     std::cout << "hello world!" << std::endl;
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800,600), "Submarines!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    sf::Font font;
+    if (!font.loadFromFile("../assets/fonts/LiberationSans-Regular.ttf")){
+        ERR("unable to load font");
+    }
+
+    sf::Text text;
+    text.setFont(font);
+
+    text.setString("This is a game about submarines.");
+    text.setCharacterSize(24);
+    text.setColor(sf::Color::Red);
+    // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    // inside the main loop, between window.clear() and window.display()
+    while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        // window.draw(shape);
+        window.draw(text);
         window.display();
+
     }
 
 
