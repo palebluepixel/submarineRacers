acacd85510b5fbfec93e2e791b04ed9254460d27
Cody Malanos Silverman <cms@uchicago.edu>
2017-04-24 19:59:07 -0500

    please

--- src/util/file.cxx
+++ src/util/file.cxx
@@ -1,9 +1,14 @@
 #include "file.hxx"
+#include <ent/Entity.hxx>
+#include <json.hpp>
+#include <glm/glm.hpp>
 
 #include <fstream>
 #include <streambuf>
 #include <sstream>
 #include <iostream>
+
+using json = nlohmann::json;
 
 char* fileio::load_file(const char* path){
 	std::ifstream t;
@@ -18,3 +23,32 @@ char* fileio::load_file(const char* path){
 	t.close();                    // close file handle
 	return buffer;
 }
+
+glm::vec3 convert3vec(double *vec) {
+    glm::vec3 v(vec[0], vec[1], vec[2]);
+    return v;
+}
+
+Entity* fileio::load_Entity(char* raw) {
+        auto j = json::parse(raw);
+        double* vec = j["initial_position"];
+        glm::vec3 initial_position;
+        glm::mat3 initial_orientation;
+        int id;
+        char *name;
+        EntityType type;
+        EntityStatus status;
+        float tick_interval;
+        /*
+         * do stuff
+         */
+        Entity e(initialPosition, initial_orientation, id, name, type, status, tick_interval);
+        return &e;
+}
+
+/*
+
+{
+    "initial_pos": [1, 2, 3],
+
+*/
--- src/util/file.hxx
+++ src/util/file.hxx
@@ -1,5 +1,7 @@
 #include <string>
+#include <ent/Entity.hxx>
 
 namespace fileio{
 	extern char* load_file(const char* path);
+        extern Entity* load_Entity(char* raw);
 };
