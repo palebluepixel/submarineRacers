396e5c0600161529749e0178bffa98a4fe6b0095
Conor Marco <conormarco@uchicago.edu>
2017-04-25 18:00:13 -0500

    Finished CollisionUtils for all line-line stuff, with optimizations.

--- src/physics/CollisionUtils.hxx
+++ src/physics/CollisionUtils.hxx
@@ -10,8 +10,6 @@ struct Segment {
     vec3 b;
 };
 
-//TODO struct Plane
-
 struct DistanceResult {
     vec3 a;
     vec3 b;
@@ -21,7 +19,5 @@ struct DistanceResult {
 DistanceResult shortestDistance(vec3 p1, vec3 p2);
 DistanceResult shortestDistance(vec3 p, Segment l);
 DistanceResult shortestDistance(Segment l1, Segment l2);
-//DistanceResult shortestDistance(vec3 p, Plane l);
-//DistanceResult shortestDistance(Segment l, Plane p);
 
 #endif
