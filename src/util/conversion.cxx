1c705da32576af3e41e289f7e971a92574aab67f
Cody Malanos Silverman <cms@uchicago.edu>
2017-05-04 17:03:57 -0500

    not even sure

--- src/util/conversion.cxx
+++ src/util/conversion.cxx
@@ -1,7 +1,7 @@
 #include "conversion.hxx"
 
 quaternion convert::quatFromSTDVec(std::vector<float> v) {
-    return quaternion(v.at(0), v.at(1), v.at(2), v.at(3));
+    return glm::angleAxis(v.at(0), vec3(v.at(1), v.at(2), v.at(3)));
 }
 
 vec3 convert::vec3FromSTDVec(std::vector<float> v) {
